df349d5bc6517e4a07ca6a28c2698c6493fecc71
Justin Thein <jthein@berkeley.edu>
2020-02-15 01:48:58 -0800

    more setup

---
+++ Chessboard.java
@@ -0,0 +1,16 @@
+/** This class represents the chessboard in which the game is played. */
+class Chessboard {
+    Piece[][] _ranks;
+    public Chessboard() {
+        _ranks = new Piece[8][8];
+    }
+    public class Piece {
+        public Piece() {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
